1bc0fe5cdca763d919f32c748795d81b2319ac5a
Wyze00 <nathantanusubroto15@gmail.com>
2026-03-03 22:18:34 +0700

    [03/03/2006]

---
+++ 1545.ts
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/find-kth-bit-in-nth-binary-string
+
+function findKthBit(n: number, k: number): string {
+    let s: string = '0';
+3
+    while (n != 1) {
+
+        if (s.length >= k) {
+            return s[k-1]!;
+        }
+
+        s = s + '1' + invertStringBit(s.split('').reverse().join(''));
+        n--;
+    }
+
+    return s[k-1]!;
+};
+
+function invertStringBit(str: string): string {
+    return str.split('').map(c => c === '1' ? '0' : '1').join('');
+}
+
+console.log(findKthBit(3,1));
